5e645e4c2e07d8128e0e2c82b5bae0cf725c5be5
Manuel Higuera <manuel.higuera@alumno.udg.mx>
2019-02-06 23:15:27 -0600

    Comenzando Repositorio

---
+++ main.py
@@ -0,0 +1,53 @@
+#p3 install )Importar las dos librerias
+import requests
+from bs4 import BeautifulSoup
+import json
+
+#guardar url en una variable
+url = "http://aviso.informador.com.mx/index.php/bienes-raices"
+
+r = requests.get(url)
+r.encoding = 'utf-8'
+#print(r.text)
+
+soup = BeautifulSoup(r.text, 'html.parser')
+items = soup.find_all(class_='items')
+# print(items)
+casas = items[0].find_all('li')
+print(casas)
+
+lista = []
+for c in casas:
+    casa = {
+        "ubicacion": c.find_all(class_='location')[0].text,
+        "titulo": c.a.text,
+        "precio": c.h5.text,
+        "descripcion": c.p.text,
+        "recamaras": c.find(class_='info-rec').text,
+        "m2": c.find(class_='info-m2').text,
+        "m2_2": c.find(class_='info-m2-2').text,
+        "wc": c.find(class_='info-wc').text,
+        "cars": c.find(class_='info-cars').text,
+        "colonia": c.find(class_='info-gps').contents[1],
+        "imgs": ['http:' + i['src'] for i in c.find_all('img')]
+    }
+    lista.append(casa)
+
+# # print(lista)
+#
+with open('informador.json','w') as archivo:
+    json.dump(lista, archivo, sort_keys = False, indent = 4)
+
+# c = casas[0]
+# ubicacion = c.find_all(class_='location')[0].text
+# titulo = c.a.text
+# precio = c.h5.text
+# descripcion = c.p.text
+# recamaras =  c.find(class_='info-rec').text
+# m2 = c.find(class_='info-m2').text
+# m2_2 = c.find(class_='info-m2-2').text
+# wc = c.find(class_='info-wc').text
+# cars = c.find(class_='info-cars').text
+# colonia = c.find(class_='info-gps').contents[1]
+# imgs = ['http:' + i['src'] for i in c.find_all('img')]
+
