135bd727b7f41b28e0d200fb005308bb79de885b
Manuel Higuera <manuel.higuera@alumno.udg.mx>
2019-03-06 13:51:13 -0600

    Se agrego el script para inserccion de las imagenes a la base de datos

--- lector.py
+++ lector.py
@@ -57,10 +57,18 @@ def insertar_fecha(file):
     conexion.commit()
     return cursor.lastrowid
 
+def insertar_imagen(url, id_casa):
+    insert = 'INSERT INTO imagen(ubicacion, id_bienraiz) VALUES (%s, %s)'
+    cursor.execute(insert, (url, id_casa))
+    conexion.commit()
+
 def insertar_bienraiz(casa, id_colonia, id_fecha):
     insert = 'INSERT INTO bienraiz(titulo, precio, m2, rooms, baths, cars, descripcion, id_tipo, id_origen, id_colonia, id_fecha) VALUES(%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s)'
     cursor.execute(insert, (casa['titulo'], casa['precio'], casa['m2'], casa['recamaras'], casa['wc'], casa['cars'], casa['descripcion'], tipo[casa['tipo']], 1, id_colonia, id_fecha,))
     conexion.commit()
+    id_casa = cursor.lastrowid
+    for img in casa['imgs']:
+        insertar_imagen(img, id_casa)
 
 def get_id_colonia(colonia, municipio):
     select = 'SELECT * from colonia WHERE nombre = %s'
@@ -81,7 +89,6 @@ def get_id_municipio(municipio):
     cursor.execute(select, (municipio,))
     rows = cursor.fetchall()
     return rows[0][0]
-
 
 for file in files:
     with open(file, encoding='utf-8') as f:
